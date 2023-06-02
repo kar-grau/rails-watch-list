09ae317020e9a8eca010dfe827a50429d52a775f
Karina Basurto <kar.basurto@hotmail.com>
2023-06-02 23:26:05 +0100

    made changes in index show new

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,7 +14,11 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    @list.save
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
@@ -22,5 +26,5 @@ class ListsController < ApplicationController
   def list_params
     params.require(:list).permit(:name)
   end
-  
+
 end
